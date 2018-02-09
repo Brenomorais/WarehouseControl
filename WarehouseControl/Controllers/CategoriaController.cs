2155f7beec88157088032c878d2382bed843263c
Breno Morais <breno.lopes.morais@hotmail.com>
2018-02-09 06:26:21 -0200

    Add project files.

---
+++ WarehouseControl/Controllers/CategoriaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaelumEstoque.Models;
+using CaelumEstoque.DAO;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categoria
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+               
+
+        [HttpPost]
+        public ActionResult Adiciona(CategoriaDoProduto categoria)
+        {            
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(categoria);
+
+            //return View(); // Redireciona para a view com o nome do metodo "Adiciona" /Categoria/Adiciona
+            return RedirectToAction("Index", "Categoria");
+        }
+    }
+}
---
+++ WarehouseControl/Filters/AutorizacaoFilterAtribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CaelumEstoque.Filters
+{
+    public class AutorizacaoFilterAtribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.Controller.GetType().Name != "LoginController")
+            {
+                object usuarioLogado = filterContext.HttpContext.Session["usuarioLogado"];
+                if (usuarioLogado == null)
+                {
+                    filterContext.Result = new RedirectToRouteResult(
+                        new RouteValueDictionary(
+                            new { action = "Index", controller = "Login" }));
+                }
+            }
+        }
+
+    }
+}
